b7494ffba237c9d85223b74cdedbfbb485810f81
Austin Wang <62589566+wangaustin@users.noreply.github.com>
2021-11-02 16:03:35 -0500

    Add files via upload

---
+++ Project2-Austin/conn.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbhost = '127.0.0.1'; // localhost
+$dbuname = 'root';
+$dbpass = 'root';
+$dbname = 'RecipeDB'; //Database name
+
+$dbo = new PDO('mysql:host=' . $dbhost . ';port=8889;dbname=' . $dbname, $dbuname, $dbpass);
+
+?>
