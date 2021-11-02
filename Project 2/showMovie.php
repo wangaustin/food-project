af58abdc1404b36c30225813eeed9f34d3dc7aae
Austin Wang <62589566+wangaustin@users.noreply.github.com>
2021-11-02 13:48:43 -0500

    Initial commit

---
+++ Project 2/showMovie.php
@@ -0,0 +1,110 @@
+<?php
+// If the all the variables are set when the Submit button is clicked...
+if (isset($_POST['field_submit'])) {
+    // Refer to conn.php file and open a connection.
+    require_once("conn.php");
+    // Save the query into variable called $query. Note that :ph_director is a place holder
+    $query = "SELECT * FROM Movie";
+
+try
+    {
+      // Create a prepared statement. Prepared statements are a way to eliminate SQL INJECTION.
+      $prepared_stmt = $dbo->prepare($query);
+      //bind the value saved in the variable $var_director to the place holder :ph_director  
+      // Use PDO::PARAM_STR to sanitize user string.
+      $prepared_stmt->execute();
+      // Fetch all the values based on query and save that to variable $result
+      $result = $prepared_stmt->fetchAll();
+
+    }
+    catch (PDOException $ex)
+    { // Error in database processing.
+      echo $sql . "<br>" . $error->getMessage(); // HTTP 500 - Internal Server Error
+    }
+}
+?>
+
+<html>
+<!-- Any thing inside the HEAD tags are not visible on page.-->
+  <head>
+    <!-- THe following is the stylesheet file. The CSS file decides look and feel -->
+    <link rel="stylesheet" type="text/css" href="project.css" />
+  </head> 
+<!-- Everything inside the BODY tags are visible on page.-->
+  <body>
+    <div id="navbar">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="getMovie.php">Search Movie</a></li>
+        <li><a href="insertMovie.php">Insert Movie</a></li>
+        <li><a href="deleteMovie.php">Delete Movie</a></li>
+        <li><a href="showMovie.php">All Movies</a></li>
+      </ul>
+    </div>
+    
+    <h1>Show All Movies</h1>
+    <!-- This is the start of the form. This form has one text field and one button.
+      See the project.css file to note how form is stylized.-->
+    <form method="post">
+
+      <label for="id_director">Click Here</label>
+      <!-- The input type is a submit button. Note the name and value. The value attribute decides what will be dispalyed on Button. In this case the button shows Submit.
+      The name attribute is referred  on line 3 and line 61. -->
+      <input type="submit" name="field_submit" value="Show All Movies">
+    </form>
+    
+    <?php
+      if (isset($_POST['field_submit'])) {
+        // If the query executed (result is true) and the row count returned from the query is greater than 0 then...
+        if ($result && $prepared_stmt->rowCount() > 0) { ?>
+              <!-- first show the header RESULT -->
+              <h2>Results</h2>
+              <!-- THen create a table like structure. See the project.css how table is stylized. -->
+              <table>
+                <!-- Create the first row of table as table head (thead). -->
+                <thead>
+                   <!-- The top row is table head with four columns named -- ID, Title ... -->
+                  <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Year</th>
+                    <th>Director</th>
+                  </tr>
+                </thead>
+                 <!-- Create rest of the the body of the table -->
+                <tbody>
+                   <!-- For each row saved in the $result variable ... -->
+                  <?php foreach ($result as $row) { ?>
+                
+                    <tr>
+                       <!-- Print (echo) the value of mID in first column of table -->
+                      <td><?php echo $row["mID"]; ?></td>
+                      <!-- Print (echo) the value of title in second column of table -->
+                      <td><?php echo $row["title"]; ?></td>
+                      <!-- Print (echo) the value of movieYear in third column of table and so on... -->
+                      <td><?php echo $row["movieYear"]; ?></td>
+                      <td><?php echo $row["director"]; ?></td>
+                    <!-- End first row. Note this will repeat for each row in the $result variable-->
+                    </tr>
+                  <?php } ?>
+                  <!-- End table body -->
+                </tbody>
+                <!-- End table -->
+            </table>
+  
+        <?php } else { ?>
+          <!-- IF query execution resulted in error display the following message-->
+          <h3>Sorry, no results found for director <?php echo $_POST['field_director']; ?>. </h3>
+        <?php }
+    } ?>
+
+
+    
+  </body>
+</html>
+
+
+
+
+
+
