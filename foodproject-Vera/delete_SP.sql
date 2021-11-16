d8fda68d25067f496899400d64a473b41f42b90c
verayang01 <85503465+verayang01@users.noreply.github.com>
2021-11-16 17:21:18 -0600

    Update delete_SP.sql

--- foodproject-Vera/delete_SP.sql
+++ foodproject-Vera/delete_SP.sql
@@ -1,4 +1,6 @@
--- stored procedure that deletes a review from the database
+USE recipedb;
+
+-- transaction and stored procedure that deletes a review from the database
 DROP PROCEDURE IF EXISTS delete_review;
 DELIMITER //
 CREATE PROCEDURE delete_review(IN review_id_in INT)
@@ -7,9 +9,16 @@ BEGIN
     DECLARE cur_recipe_id INT;
     DECLARE total_rating INT;
     DECLARE num_rating INT;
+    DECLARE review_nonexist INT DEFAULT FALSE;
 	DECLARE sql_error INT DEFAULT FALSE;
 	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET sql_error = TRUE;
     START TRANSACTION;
+    
+    -- check if the review_id exists
+    IF review_id_in NOT IN 
+		(SELECT review_id FROM review) THEN
+    SET review_nonexist = TRUE;
+    END IF;
     
     SET cur_recipe_id = 
 		(SELECT recipe_id
@@ -18,7 +27,6 @@ BEGIN
     
 	DELETE FROM review
     WHERE review_id = review_id_in;
-        
          
 	-- decrement review_count in recipe table
 	UPDATE recipe
@@ -41,20 +49,77 @@ BEGIN
     SET aggregated_rating = ROUND(CEILING(FLOOR((total_rating / num_rating) * 4) / 2)/2, 2)
     WHERE recipe_id = cur_recipe_id;
             
-	IF sql_error = FALSE THEN
-    COMMIT;
+	IF sql_error = FALSE AND review_nonexist = TRUE THEN
+    ROLLBACK;
+    SELECT 'Review does not exist.';
+	ELSEIF sql_error = FALSE AND review_nonexit = FALSE THEN
+	COMMIT;
 	SELECT 'Review successfully deleted.';
 	ELSE 
     ROLLBACK;
-	SELECT 'Action failed. Cannot delete review. ';
+	SELECT 'Action failed. Cannot delete review.';
 	END IF;
 
 END//
 DELIMITER ;
-
 
 -- Sample call to delete_review
 CALL delete_review(2090361);
 SELECT * FROM review ORDER BY review_id DESC;
 SELECT recipe_id, recipe_name, author_id, aggregated_rating, review_count
 FROM recipe WHERE recipe_id = 541386;
+
+
+
+-- trigger that deletes reviews on a recipe before the recipe itself is deleted 
+DROP TRIGGER IF EXISTS recipe_before_delete;
+DELIMITER //
+CREATE TRIGGER recipe_before_delete
+BEFORE DELETE
+ON recipe
+FOR EACH ROW
+BEGIN
+	
+    DELETE FROM recipedb.review
+    WHERE recipe_id = OLD.recipe_id;
+
+END //
+DELIMITER ;
+
+-- stored procedure that deletes a recipe from the database
+DROP PROCEDURE IF EXISTS delete_recipe;
+DELIMITER //
+CREATE PROCEDURE delete_recipe(IN recipe_id_in INT)
+BEGIN
+	
+    DECLARE recipe_nonexist INT DEFAULT FALSE;
+	DECLARE sql_error INT DEFAULT FALSE;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET sql_error = TRUE;
+    
+    -- check if the recipe_id exists
+	IF recipe_id_in NOT IN 
+		(SELECT recipe_id FROM recipe) THEN
+	SET recipe_nonexist = TRUE;
+	END IF;
+    
+    DELETE FROM recipe
+    WHERE recipe_id = recipe_id_in;
+    
+	IF sql_error = FALSE AND recipe_nonexist = TRUE THEN
+    SELECT 'Recipe does not exist.';
+	ELSEIF sql_error = FALSE AND recipe_nonexit = FALSE THEN
+	SELECT 'Recipe successfully deleted.';
+	ELSE 
+	SELECT 'Action failed. Cannot delete recipe.';
+	END IF;
+    
+
+END//
+DELIMITER ;
+
+-- Sample call to delete_recipe
+CALL delete_recipe(541386);
+SELECT recipe_id, recipe_name, author_id, aggregated_rating, review_count
+FROM recipe WHERE recipe_id = 541386;
+SELECT * FROM review WHERE recipe_id = 541386;
+CALL delete_recipe(541386);
