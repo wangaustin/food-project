bec31646eaae51a3326863bb0e23864f2b06fd8a
verayang01 <85503465+verayang01@users.noreply.github.com>
2021-12-02 16:55:30 -0600

    Add files via upload

---
+++ foodproject-Vera/index.php
@@ -0,0 +1,21 @@
+<html>
+<!-- Any thing inside the HEAD tags are not visible on page.-->
+  <head>
+    <!-- THe following is the stylesheet file. The CSS file decides look and feel -->
+    <link rel="stylesheet" type="text/css" href="project.css" />
+  </head> 
+<!-- Everything inside the BODY tags are visible on page.-->
+  <body>
+    <div id="navbar">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="getRecipe.php">Search Recipe</a></li>
+        <li><a href="insertRecipe.php">Insert Recipe</a></li>
+        <li><a href="deleteRecipe.php">Delete Recipe</a></li>
+        <li><a href="showRecipe.php">All Recipe</a></li>
+      </ul>
+    </div>
+    <h1>Food N Recipes</h1>
+	<p>
+  </body>
+</html>
