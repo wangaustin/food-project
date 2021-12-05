05e72eecb18ce328c0df2cc879ad3279c59f3267
verayang01 <85503465+verayang01@users.noreply.github.com>
2021-12-05 02:46:34 -0600

    Add files via upload

---
+++ vera-code/delete_SP.sql
@@ -0,0 +1,122 @@
+USE recipedb;
+
+-- stored procedure that deletes a review from the database
+DROP PROCEDURE IF EXISTS delete_review;
+DELIMITER //
+CREATE PROCEDURE delete_review(IN review_id_in INT)
+BEGIN
+	
+    DECLARE cur_recipe_id INT;
+    DECLARE total_rating INT;
+    DECLARE num_rating INT;
+    DECLARE review_nonexist INT DEFAULT FALSE;
+	DECLARE sql_error INT DEFAULT FALSE;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET sql_error = TRUE;
+    
+    -- check if the review_id exists
+    IF review_id_in NOT IN 
+		(SELECT review_id FROM review) THEN
+    SET review_nonexist = TRUE;
+    END IF;
+    
+    SET cur_recipe_id = 
+		(SELECT recipe_id
+         FROM review
+         WHERE review_id = review_id_in);
+    
+	DELETE FROM review
+    WHERE review_id = review_id_in;
+         
+	-- decrement review_count in recipe table
+	UPDATE recipe
+    SET review_count = review_count - 1
+    WHERE recipe_id = cur_recipe_id;
+    
+    -- update aggregated_rating in recipe table
+    SET total_rating = 
+		(SELECT SUM(rating)
+         FROM review
+         WHERE recipe_id = cur_recipe_id);
+         
+	SET num_rating =
+		(SELECT COUNT(rating)
+         FROM review
+         WHERE recipe_id = cur_recipe_id);
+    
+    -- round average rating to the nearest 0.5
+    UPDATE recipe
+    SET aggregated_rating = ROUND(CEILING(FLOOR((total_rating / num_rating) * 4) / 2)/2, 2)
+    WHERE recipe_id = cur_recipe_id;
+            
+	IF sql_error = FALSE AND review_nonexist = TRUE THEN
+    SELECT 'Review does not exist.';
+	ELSEIF sql_error = FALSE AND review_nonexit = FALSE THEN
+	SELECT 'Review successfully deleted.';
+	ELSE 
+	SELECT 'Action failed. Cannot delete review.';
+	END IF;
+
+END//
+DELIMITER ;
+
+-- Sample call to delete_review and queries for testing
+CALL delete_review(1124704);
+SELECT * FROM review ORDER BY review_id DESC LIMIT 100;
+SELECT * FROM review WHERE review_id = 1124704;
+SELECT recipe_id, recipe_name, author_id, aggregated_rating, review_count
+FROM recipe WHERE recipe_id = 541387;
+
+
+-- trigger that deletes reviews on a recipe before the recipe itself is deleted 
+DROP TRIGGER IF EXISTS recipe_before_delete;
+DELIMITER //
+CREATE TRIGGER recipe_before_delete
+BEFORE DELETE
+ON recipe
+FOR EACH ROW
+BEGIN
+	
+    DELETE FROM recipedb.review
+    WHERE recipe_id = OLD.recipe_id;
+
+END //
+DELIMITER ;
+
+-- stored procedure that deletes a recipe from the database
+DROP PROCEDURE IF EXISTS delete_recipe;
+DELIMITER //
+CREATE PROCEDURE delete_recipe(IN recipe_id_in INT)
+BEGIN
+	
+    DECLARE recipe_nonexist INT DEFAULT FALSE;
+	DECLARE sql_error INT DEFAULT FALSE;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET sql_error = TRUE;
+    
+    -- check if the recipe_id exists
+	IF recipe_id_in NOT IN 
+		(SELECT recipe_id FROM recipe) THEN
+	SET recipe_nonexist = TRUE;
+	END IF;
+    
+    DELETE FROM recipe
+    WHERE recipe_id = recipe_id_in;
+    
+	IF sql_error = FALSE AND recipe_nonexist = TRUE THEN
+    SELECT 'Recipe does not exist.';
+	ELSEIF sql_error = FALSE AND recipe_nonexit = FALSE THEN
+	SELECT 'Recipe successfully deleted.';
+	ELSE 
+	SELECT 'Action failed. Cannot delete recipe.';
+	END IF;
+    
+
+END//
+DELIMITER ;
+
+-- Sample call to delete_recipe and queries for testing
+CALL delete_recipe(541386);
+SELECT recipe_id, recipe_name, author_id, aggregated_rating, review_count
+FROM recipe WHERE recipe_id = 541386;
+SELECT * FROM review WHERE recipe_id = 541386;
+CALL delete_recipe(541386);
+
---
+++ vera-code/search_SP.sql
@@ -0,0 +1,145 @@
+USE recipedb;
+
+-- stored procedure that shows the recipe search results by time in ASC/DESC order
+DROP PROCEDURE IF EXISTS show_by_time;
+DELIMITER //
+CREATE PROCEDURE show_by_time(IN keyword VARCHAR(50), IN ordering INT, IN num_rows INT)
+BEGIN
+
+IF ordering = 1 THEN
+	SELECT *
+	FROM recipe 
+	WHERE recipe_name LIKE CONCAT('%', keyword, '%')
+	ORDER BY date_published
+	LIMIT num_rows;
+ELSEIF ordering = 0 THEN		
+	SELECT *
+	FROM recipe 
+	WHERE recipe_name LIKE CONCAT('%', keyword, '%')
+	ORDER BY date_published DESC
+	LIMIT num_rows;
+END IF;
+
+
+END//
+DELIMITER ;
+
+
+-- stored procedure that shows the recipe search results by calories in ASC/DESC order
+DROP PROCEDURE IF EXISTS show_by_calories;
+DELIMITER //
+CREATE PROCEDURE show_by_calories(IN keyword VARCHAR(50), IN ordering INT, IN num_rows INT)
+BEGIN
+
+IF ordering = 1 THEN
+	SELECT *
+	FROM recipe 
+    WHERE recipe_name LIKE CONCAT('%', keyword, '%')
+	ORDER BY calories
+	LIMIT num_rows;
+ELSEIF ordering = 0 THEN
+	SELECT *
+	FROM recipe 
+    WHERE recipe_name LIKE CONCAT('%', keyword, '%')
+	ORDER BY calories DESC
+	LIMIT num_rows;
+END IF;
+
+END//
+DELIMITER ;
+
+
+-- stored procedure that shows the reviews on a particular recipe
+DROP PROCEDURE IF EXISTS show_reviews;
+DELIMITER //
+CREATE PROCEDURE show_reviews(IN current_recipe_id INT)
+BEGIN
+
+SELECT r.review_id, r.recipe_id, a.author_id, a.author_name, 
+	   r.rating, r.review, r.date_submitted, r.date_modified
+FROM review r
+	JOIN author a ON r.author_id = a.author_id
+WHERE r.recipe_id = current_recipe_id;
+
+END//
+DELIMITER ;
+
+
+-- stored procedure that search by author name
+DROP PROCEDURE IF EXISTS show_for_author;
+DELIMITER //
+CREATE PROCEDURE show_for_author(IN author_name_in VARCHAR(50))
+BEGIN
+
+SELECT *
+FROM recipe r
+	JOIN author a ON r.author_id = a.author_id
+WHERE a.author_name LIKE CONCAT('%', author_name_in, '%');
+
+END//
+DELIMITER ;
+
+-- Sample calls to the above procedures
+CALL show_by_time('Blueberry', 1, 5);
+CALL show_by_time('Blueberry', 1, -1);
+CALL show_by_time('Blueberry', 1, 4000);
+CALL show_by_calories('Blueberry', 0, 10);
+CALL show_reviews(27430);
+CALL show_for_author('Sue M.');
+
+
+-- view that shows the trending recipes (highly-rated or mostly-reviewed)
+DROP VIEW IF EXISTS trending_recipe;
+CREATE VIEW trending_recipe AS
+SELECT * 
+FROM 
+	((SELECT rp.recipe_name, a.author_name, rp.recipe_description, 
+		    rp.aggregated_rating AS average_rating, rp.calories
+     FROM author a
+		JOIN recipe rp ON a.author_id = rp.author_id
+	 WHERE rp.review_count >= 5
+     ORDER BY rp.aggregated_rating DESC
+	 LIMIT 50)
+     UNION
+	(SELECT rp.recipe_name, a.author_name, rp.recipe_description, 
+		   rp.aggregated_rating AS average_rating, rp.calories
+    FROM author a
+		JOIN recipe rp ON a.author_id = rp.author_id
+	WHERE rp.aggregated_rating >= 4.5
+    ORDER BY rp.review_count DESC
+    LIMIT 50)) union_tb
+ORDER BY recipe_name;
+
+-- Sample select from view trending_recipes
+SELECT * FROM trending_recipe;
+
+
+-- view that shows the top authors who write the most recipes and have highest ratings
+DROP VIEW IF EXISTS top_author;
+CREATE VIEW top_author AS
+SELECT *
+FROM 
+	((SELECT a.author_name, 
+		   COUNT(r.recipe_id) AS number_of_recipes, 
+           ROUND(AVG(r.aggregated_rating), 1) AS average_rating
+    FROM author a 
+		JOIN recipe r ON a.author_id = r.author_id
+	GROUP BY a.author_id
+    HAVING average_rating >= 4
+    ORDER BY number_of_recipes DESC
+    LIMIT 15)
+    UNION
+    (SELECT a.author_name, 
+		   COUNT(r.recipe_id) AS number_of_recipes, 
+           ROUND(AVG(r.aggregated_rating), 1) AS average_rating
+    FROM author a
+		JOIN recipe r ON a.author_id = r.author_id
+	GROUP BY a.author_id
+    HAVING number_of_recipes >= 20
+    ORDER BY average_rating DESC
+    LIMIT 15)) union_tb
+ORDER BY author_name;
+
+-- Sample select from view top_author	
+SELECT * FROM top_author;
+
